f91c1d4a659d495f129f6deb01d618d0c8af83e7
Anthony Liang <Anthony@appacademy.io>
2014-01-09 17:52:09 -0500

    added Game class but turns aren't working

--- checkers.rb
+++ checkers.rb
@@ -30,7 +30,7 @@ class Game
       @board.render
       puts "it's #{color}'s turn!"
       turn
-      win?
+      break if win?
       color == :r ? color = :w : color = :r
     end
 
@@ -38,48 +38,45 @@ class Game
   end
 
   def win?
-    remaining_pieces = @board.board.map {|row| row}
-    remaining_pieces = remaining_pieces.flatten.compact
+    # win if the only pieces left on the board are the same color
+    current_board = @board.board.map {|row| row}
+    remaining_pieces = current_board.flatten.compact
     remaining_pieces.all? { |piece| piece.color == color }
   end
 
   def turn
-    begin
-      # starting position
+
+
+      # get starting position
       puts "Which piece would you like to move? (ex. b6)"
       move_start = gets.chomp.downcase.split("")
       move_start = [KEY_MAP[move_start.last], move_start.first.ord - "a".ord]
 
-      # end position
+      #get move(s)
       puts "Where would you like to move it? (ex. a5)"
+      puts "You can add multiple spots, just enter f when finished"
       move_end = gets.chomp.downcase.split("")
       move_end = [KEY_MAP[move_end.last], move_end.first.ord - "a".ord]
-
-      # put end position into move_sequence
       @move_sequence << move_end
+      puts "Got it!"
 
-      # prompt for additional input (optional)
-      puts "You can add multiple spots, just enter f when finished"
-      until gets.chomp == "f"
-        move_end = gets.chomp.downcase.split("")
-        move_end = [KEY_MAP[move_end.last], move_end.first.ord - "a".ord]
-        @move_sequence << move_end
-        puts "Got it!"
-        return
-      end
+
+      @board[move_start].perform_moves(@move_sequence)
+      @move_sequence = []
+
 
       # if @board[move_start].color != color
       #   @move_sequence = []
       # end
 
-      @board[move_start].perform_moves(@move_sequence)
-      @move_sequence = []
+      # @board[move_start].perform_moves(@move_sequence)
+      # @move_sequence = []
     # rescue
     #   puts "That sequence was garbage. Try again!".colorize(:red)
     #   puts "Did you make sure you picked your own piece?".colorize(:red)
     #   render
     #   retry
-    end
+    # end
 
   end
 
